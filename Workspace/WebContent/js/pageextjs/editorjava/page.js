afbf850017343f12be8c8d7b8049acd44c3c510d
M6C <david.roca@free.fr>
2016-09-16 13:46:08 +0200

    Add ExtJs 4.0.7 Example

--- Workspace/WebContent/js/pageextjs/editorjava/page.js
+++ Workspace/WebContent/js/pageextjs/editorjava/page.js
@@ -3,16 +3,17 @@ function init_page() {
 	Ext.Loader.setPath('Workspace.editorjava', DOMAIN_NAME_ROOT + '/js/pageextjs/editorjava');
 
 // DEPENDENCE
-	Ext.Loader.setConfig({enabled: true});
-
+	Ext.Loader.setConfig({enabled: true, origine:DOMAIN_NAME_ROOT});
+	Ext.Loader.setPath('Ext.ux', DOMAIN_NAME_ROOT + '/jsFramework/ext-4.0.7/examples/ux');
 //	Ext.Loader.setPath('Ext.ux', '../ux/');
-	Ext.Loader.setPath("Ext.ux", "http://cdn.sencha.io/ext-4.0.7-gpl/examples/ux");
+//	Ext.Loader.setPath("Ext.ux", "http://cdn.sencha.io/ext-4.0.7-gpl/examples/ux");
+//	Ext.Loader.setPath("Ext.ux", "http://plus.xsense.co.th/ext-4.0.7/examples/ux");
 
 	Ext.require([
 	    'Ext.data.*',
 	    'Ext.grid.*',
 	    'Ext.util.*',
-	    'Ext.ux.data.PagingMemoryProxy',
+	    'Ext.ux.PagingMemoryProxy',
 	    'Ext.ux.ProgressBarPager'
 	]);
 
