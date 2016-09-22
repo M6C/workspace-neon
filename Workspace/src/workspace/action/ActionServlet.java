7e9eda23f04e2df55a33bff46a2ed7fb1a093546
M6C <david.roca@free.fr>
2016-09-22 22:53:53 +0200

    add Workspace Action Servlet clear default WorkSpace_Security.xml

--- Workspace/src/workspace/action/ActionServlet.java
+++ Workspace/src/workspace/action/ActionServlet.java
@@ -11,12 +11,12 @@ public class ActionServlet extends framework.action.ActionServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private static String PARAMETER_WORKSPACE_SECURITY_XML = "workspace.security.xml";
+	private static String PARAMETER_WORKSPACE_SECURITY = "workspace.security";
 
 	//Initialize global variables
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		String szSecurityXml = System.getProperty(PARAMETER_WORKSPACE_SECURITY_XML);
+		String szSecurityXml = System.getProperty(PARAMETER_WORKSPACE_SECURITY);
 		if (UtilString.isNotEmpty(szSecurityXml)) {
 //			try {WORKSPACE_SECURITY_XML = getServletContext().getRealPath(szSecurityXml);}catch (Exception ex){}
 			try {WORKSPACE_SECURITY_XML = new File(szSecurityXml).getCanonicalPath();}catch (Exception ex){ex.printStackTrace();}
