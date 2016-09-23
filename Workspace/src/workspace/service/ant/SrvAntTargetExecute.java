333d880a95f67d06324582514df9f23981e60720
M6C <david.roca@free.fr>
2016-09-23 11:08:22 +0200

    Refactor Service Ant Target Execute

--- Workspace/src/workspace/service/ant/SrvAntTargetExecute.java
+++ Workspace/src/workspace/service/ant/SrvAntTargetExecute.java
@@ -9,7 +9,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.net.URL;
 import java.util.Hashtable;
 import java.util.Vector;
 
@@ -17,6 +16,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.transform.TransformerException;
 
 import org.apache.tools.ant.BuildLogger;
 import org.apache.tools.ant.NoBannerLogger;
@@ -67,52 +67,14 @@ public class SrvAntTargetExecute extends SrvGenerique
 	            //Recuperation du chemin des sources
 	            String szPathSource = AdpXmlApplication.getPathByName(context, domXml, application, "Source");
 	            szPathSource = (szPathSource == null) ? szPathSource : UtilPath.formatPath(domXml, szPathSource);
+	            szPathSource = (new File(szPathMain, szPathSource)).getCanonicalPath();
 	            //Recuperation du chemin de destination des classes
 	            String szPathClass = AdpXmlApplication.getPathByName(context, domXml, application, "Class");
 	            szPathClass = (szPathClass == null) ? szPathClass : UtilPath.formatPath(domXml, szPathClass);
-	            //Recuperation des classpath de l'application
-	            String szClasspath = AdpXmlApplication.getClassPathAll(context, domXml, application);
-	            szClasspath = (szClasspath == null) ? szClasspath : UtilPath.formatPath(domXml, szClasspath);
-
-	            //Recuperation de la home du jdk
-	            String szJdkpath = AdpXmlApplication.getJdkPathByName(context, domXml, application, "Home");
-	            // Recuperation du repertoire home de la jre
-	            String szJreHome = AdpXmlApplication.getJdkJrePathByName(context, domXml, application, "Home");
-	            // Recuperation du repertoire lib de la jre
-	            String szJreLib = AdpXmlApplication.getJdkJrePathByName(context, domXml, application, "Lib");
-
-	            szPathSource = (new File(szPathMain, szPathSource)).getCanonicalPath();
 	            szPathClass = (new File(szPathMain, szPathClass)).getCanonicalPath();
-	            StringBuffer pathClass = new StringBuffer(UtilPackage.getPackageClassPath());
-	            pathClass.append(";").append(szClasspath);
-	            addJarToClassPath(context.getRealPath("WEB-INF"), pathClass);
-	            if(UtilString.isNotEmpty(szJdkpath))
-	            {
-	                File jdkPath = new File(szJdkpath);
-	                if(jdkPath.exists()) {
-	                    if(UtilString.isNotEmpty(szJreHome)) {
-	                    	File jreHome = szJreHome.indexOf(':') <= 0 ? new File(jdkPath, szJreHome) : new File(szJreHome);
-	                    	if (jreHome.exists()) {
-		                    	File jreLib = new File(jreHome, "lib");
-		                        if (jreLib.exists()) {
-		                            addJarToClassPath(jreLib.getCanonicalPath(), pathClass);
-		                        }
-	                    	}
-	                    }
-	                    if(UtilString.isNotEmpty(szJreLib)) {
-	                    	File jreLib = szJreLib.indexOf(':') <= 0 ? new File(jdkPath, szJreLib) : new File(szJreLib);
-	                        if(jreLib.exists()) {
-	                            addJarToClassPath(jreLib.getCanonicalPath(), pathClass);
-	                        }
-	                    }
-	                }
-	            }
+	            //Recuperation des classpath de l'application
+	            StringBuffer pathClass = getClassPath(context, application, domXml);
 
-//	            URL buildXml = context.getResource("/Xml/Ant/Task/CompileProject.xml");
-//	            Project p = new Project();
-//	            p.setUserProperty("ant.file", buildXml.getPath());
-
-//	            String buildXml = new File(context.getRealPath("/Xml/Ant/Task/CompileProject.xml")).getAbsolutePath();
 	            File buildXml = new File(context.getRealPath("/Xml/Ant/Task/CompileProject.xml"));
 	            Project p = new Project();
 	            p.setUserProperty("ant.file", buildXml.getAbsolutePath());
@@ -173,8 +135,46 @@ public class SrvAntTargetExecute extends SrvGenerique
 		request.setAttribute("msgText", streamLog.toString());
     }
 
-    private void addJarToClassPath(String path, StringBuffer classpath)
-        throws IOException
+	private StringBuffer getClassPath(ServletContext context, String application, Document domXml) throws TransformerException, IOException {
+		//Recuperation du ClassPath
+        String szClasspath = AdpXmlApplication.getClassPathAll(context, domXml, application);
+        szClasspath = (szClasspath == null) ? szClasspath : UtilPath.formatPath(domXml, szClasspath);
+		//Recuperation de la home du jdk
+		String szJdkpath = AdpXmlApplication.getJdkPathByName(context, domXml, application, "Home");
+		// Recuperation du repertoire lib du jdk
+		String szJdkLib = AdpXmlApplication.getJdkJrePathByName(context, domXml, application, "Lib");
+		// Recuperation du repertoire home de la jre
+		String szJreHome = AdpXmlApplication.getJdkJrePathByName(context, domXml, application, "Home");
+
+		StringBuffer pathClass = new StringBuffer(UtilPackage.getPackageClassPath());
+		pathClass.append(";").append(szClasspath);
+		addJarToClassPath(context.getRealPath("WEB-INF"), pathClass);
+		if(UtilString.isNotEmpty(szJdkpath))
+		{
+		    File jdkPath = new File(szJdkpath);
+		    if(jdkPath.exists()) {
+		        if(UtilString.isNotEmpty(szJreHome)) {
+		        	File jreHome = szJreHome.indexOf(':') <= 0 ? new File(jdkPath, szJreHome) : new File(szJreHome);
+		        	if (jreHome.exists()) {
+		            	File jreLib = new File(jreHome, "lib");
+		                if (jreLib.exists()) {
+		                    addJarToClassPath(jreLib.getCanonicalPath(), pathClass);
+		                }
+		        	}
+		        }
+		        if(UtilString.isNotEmpty(szJdkLib)) {
+		        	File jreLib = szJdkLib.indexOf(':') <= 0 ? new File(jdkPath, szJdkLib) : new File(szJdkLib);
+		            if(jreLib.exists()) {
+		                addJarToClassPath(jreLib.getCanonicalPath(), pathClass);
+		            }
+		        }
+		    }
+		}
+		return pathClass;
+	}
+
+
+    private void addJarToClassPath(String path, StringBuffer classpath) throws IOException
     {
         Vector listJar = UtilFile.dir(path, true, ".jar");
         int max = UtilVector.safeSize(listJar);
