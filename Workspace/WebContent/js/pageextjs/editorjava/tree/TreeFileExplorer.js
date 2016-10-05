e8ccbc382f44e67f3d041d5db0167348ade3f70e
M6C <david.roca@free.fr>
2016-10-05 15:07:52 +0200

    Upd Editor Java Focus Tree File Explorer 1st Node - Fix (Method Not Used but Perhaps Useful one day)

--- Workspace/WebContent/js/pageextjs/editorjava/tree/TreeFileExplorer.js
+++ Workspace/WebContent/js/pageextjs/editorjava/tree/TreeFileExplorer.js
@@ -20,14 +20,50 @@ Ext.define('Workspace.editorjava.tree.TreeFileExplorer', {
 		'load' : function(store, records, successful, operation, eOpts) {
 			if (successful) {
 				var view = this.getView();
-				var node = view.panel.getRootNode();
-				if (node.firstChild != undefined) {
-					node = node.firstChild;
+				var node = records;
+				if (node.parentNode == undefined && node.firstChild != undefined) {
+					view.select(node.firstChild);
 				}
-				view.select(node);
 				view.focus();
 			}
 		}
 	}
-
+	,
+	// Not Used
+	selectNodeFromViewRoot: function () {
+		var view = this.getView();
+		var node = view.panel.getRootNode();
+		if (node.firstChild != undefined) {
+			node = node.firstChild;
+		}
+		view.select(node);
+		view.focus();
+	}
+	,
+	// Not Used
+	selectNodeFromRecord: function (records) {
+		var node = records.firstChild;
+		if (node == undefined) {
+			node = view.panel.getRootNode();
+			if (node.firstChild != undefined) {
+				node = node.firstChild;
+			}
+		}
+		view.select(node);
+		view.focus();
+	}
+	,
+	// Not Used
+	getSelectPath : function(node) {
+		var n = node.parentNode;
+		var path = '';
+		while (n != undefined) {
+			if (path != '') {
+				path = '<' + path;
+			}
+			path = n.getId() + path;
+			n = n.parentNode;
+		}
+		return path;
+	}
 }, function() {Workspace.tool.Log.defined('Workspace.editorjava.tree.TreeFileExplorer');});
