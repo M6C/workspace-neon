aea14a9e3aff4776ff63295cb1268597e108d384
M6C <david.roca@free.fr>
2016-10-06 14:17:40 +0200

    Fix Editor Java Button Save And Build

--- Workspace/WebContent/js/pageextjs/editorjava/request/JsonEditSaveAndBuild.js
+++ Workspace/WebContent/js/pageextjs/editorjava/request/JsonEditSaveAndBuild.js
@@ -7,28 +7,36 @@ Ext.define('Workspace.editorjava.request.JsonEditSaveAndBuild',  {
 
 	extend: 'Workspace.editorjava.request.JsonEditSaveFile'
 	,
-	callback:function(options, success, response) { 
+    constructor: function(config) {
+		console.info('Workspace.editorjava.request.JsonEditSaveAndBuild constructor');
         var me = this;
 
-		if (me.build) {
-			Workspace.common.window.WindowWaiting.updateText('Building process...');
-			Ext.Ajax.request({
-				method:'POST',
-				url:DOMAIN_NAME_ROOT + '/action.servlet?event=JsonEditCompileProject',
-				callback:function(opts, success, response) {
-					// Explicit load required library (Mandatory for extending this class)
-					Ext.Loader.syncRequire('Workspace.common.window.WindowResultText');
+        config.callback = function(options, success, response) { 
 
-					Workspace.common.window.WindowWaiting.hide("Building complete.", 1);
+    		if (me.build) {
+    			Workspace.common.window.WindowWaiting.updateText('Building process...');
+    			Ext.Ajax.request({
+    				method:'POST',
+    				url:DOMAIN_NAME_ROOT + '/action.servlet?event=JsonEditCompileProject',
+    				callback:function(opts, success, response) {
+    					// Explicit load required library (Mandatory for extending this class)
+    					Ext.Loader.syncRequire('Workspace.common.window.WindowResultText');
 
-					var option = {response: response};
-					Ext.create('Workspace.common.window.WindowTextCompile', option).show();
-				},
-				params:{application:me.application,target:'compile',className:me.className}
-			});
-		}
-		else {
-			Workspace.common.window.WindowWaiting.hide("Saving complete.", 1);
-		}
-	},
+    					Workspace.common.window.WindowWaiting.hide("Building complete.", 1);
+
+    					var option = {response: response};
+    					Ext.create('Workspace.common.window.WindowTextCompile', option).show();
+    				},
+    				params:{application:me.application,target:'compile',className:me.className}
+    			});
+    		}
+    		else {
+    			Workspace.common.window.WindowWaiting.hide("Saving complete.", 1);
+    		}
+    	};
+
+        Ext.apply(me, config);
+
+        me.callParent();
+    }
 }, function() {Workspace.tool.Log.defined('Workspace.editorjava.request.JsonEditSaveAndBuild');});
