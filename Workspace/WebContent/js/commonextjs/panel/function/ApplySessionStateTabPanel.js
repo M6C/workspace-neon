efeb2be97a5bbc095609b4c4475ba56ca23abd6e
M6C <david.roca@free.fr>
2016-12-09 15:17:49 +0100

    Upd Editor Java Add State Manager for Tab Part 3 - Refactor

---
+++ Workspace/WebContent/js/commonextjs/panel/function/ApplySessionStateTabPanel.js
@@ -0,0 +1,41 @@
+Ext.define('Workspace.common.panel.function.ApplySessionStateTabPanel', {
+	singleton : true,
+	options : {}
+	,
+	apply: function(panel, stateId) {
+	    console.debug('Workspace.common.panel.function.ApplySessionStateTabPanel apply stateId:' + stateId);
+		Ext.apply(panel, {
+			stateful: true,
+			stateId: stateId,
+			stateEvents: ['add'], 
+			stateData: undefined,
+		    getState: function() { 
+			    console.debug('Workspace.common.panel.function.ApplySessionStateTabPanel apply getState');
+		        var s = {raw: []}; 
+		        if (Ext.isDefined(panel.items)) {
+					var i = 0;
+			        panel.items.each(function(tab) {
+						s.raw[i++] = tab.raw;
+					});
+		        }
+		        return s; 
+		    }
+			, 
+		    applyState: function(s) { 
+			    console.debug('Workspace.common.panel.function.ApplySessionStateTabPanel apply applyState');
+		        panel.stateData = s;
+		    }
+	    });
+
+		panel.on('render', function(component, option) {
+		    console.debug('Workspace.common.panel.function.ApplySessionStateTabPanel apply render');
+			if (Ext.isDefined(panel.stateData) && Ext.isDefined(panel.stateData.raw)) {
+		    	Ext.Array.each(panel.stateData.raw, function(tab) {
+		    		if (tab != null) {
+		    			Workspace.editorjava.panel.center.function.AddTabAce.call(tab);
+		    		}
+				});
+			}
+		});
+	}
+}, function() {Workspace.tool.Log.defined('Workspace.common.panel.function.ApplySessionStateTabPanel');});
--- Workspace/WebContent/js/pageextjs/editorjava/panel/center/function/ApplySessionStateCenter.js
+++ Workspace/WebContent/js/pageextjs/editorjava/panel/center/function/ApplySessionStateCenter.js
@@ -4,37 +4,8 @@ Ext.define('Workspace.editorjava.panel.center.function.ApplySessionStateCenter',
 	,
 	apply: function(panel) {
 	    console.debug('Workspace.editorjava.panel.center.function.ApplySessionStateCenter apply');
-		Ext.apply(panel, {
-			stateful: true,
-			stateId: 'Workspace.editorjava.panel.PanelCenter',
-			stateEvents: ['add'], 
-			stateData: undefined,
-		    getState: function() { 
-		        var me = this, s = {raw: []}; 
-		        if (Ext.isDefined(me.items)) {
-					var i = 0;
-			        me.items.each(function(tab) {
-						s.raw[i++] = tab.raw;
-					});
-		        }
-		        return s; 
-		    }
-			, 
-		    applyState: function(s) { 
-		        var me = this;
-		        me.stateData = s;
-		    }
-	    });
 
-		panel.on('render', function(panel, option) {
-			var me = this;
-			if (Ext.isDefined(me.stateData) && Ext.isDefined(me.stateData.raw)) {
-		    	Ext.Array.each(me.stateData.raw, function(tab) {
-		    		if (tab != null) {
-		    			Workspace.editorjava.panel.center.function.AddTabAce.call(tab);
-		    		}
-				});
-			}
-		});
+	    Ext.Loader.syncRequire('Workspace.common.panel.function.ApplySessionStateTabPanel');
+	    Workspace.common.panel.function.ApplySessionStateTabPanel.apply(panel, 'Workspace.editorjava.panel.PanelCenter');
 	}
 }, function() {Workspace.tool.Log.defined('Workspace.editorjava.panel.center.function.ApplySessionStateCenter');});
