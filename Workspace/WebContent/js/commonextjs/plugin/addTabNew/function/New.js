155941ce240877aff6064bef478737d625efdcdf
M6C <david.roca@free.fr>
2016-12-13 17:11:24 +0100

    Upd AddTabNew Refactor

--- Workspace/WebContent/js/commonextjs/plugin/addTabNew/function/New.js
+++ Workspace/WebContent/js/commonextjs/plugin/addTabNew/function/New.js
@@ -28,47 +28,52 @@ Ext.define('Workspace.common.plugin.addTabNew.function.New',  {
 	            itemPathDst = item.path;
 	        } else if (!Ext.isEmpty(item.application)) {
 	            itemPathDst = '[' + item.application + ']';
-	        }
-            if (Ext.isEmpty(itemPathDst)) {
+	        } else {
     	        var text = 'No destination panel find.';
 				Workspace.common.tool.Pop.error(me, text);
 		        return;
             }
 
-        	Ext.Msg.prompt('Create directory in \''+itemPathDst+'\'', 'Please enter a name:', function(btn, text){
-        	    if (btn == 'ok' && text != ''){
-    		        var fileName = text;
-    		    	var requestUrl = DOMAIN_NAME_ROOT + '/action.servlet?event=FileBrowserNew';
-    	  			Ext.Ajax.request({
-    	  			   url: requestUrl,
-    	  			   params: {type:typeNew, pathDst:itemPathDst, name:fileName},
-    	  			   success: function(result, request){
-    	  				   var text = 'Success creating \''+text+'\' in \''+itemPathDst+'\'';
-    	  				   Workspace.common.tool.Pop.success(me, text);
-
-    	  				   // Rechargement de la grid
-    	  				   var grid = mainCenterTab.items.items[0];
-    	  				   grid.refresh();
-
-    	  				   // Rechargement du tree si besoin
-    	  				   var application = Ext.getCmp('project').value;
-    	  				   if ('['+application+']'==itemPathDst) {
-    	  						var tree = Ext.getCmp("treeDirectory");
-    	  						tree.getStore().load(
-	  								new Ext.data.Operation({
-	  									action:'read'
-	  								})
-	  							);
-    	  				   }
-    	  			   },
-    	  			   failure: function (result, request) {
-    		  			   var jsonData = Ext.decode(result.responseText);
-    		  			   var message = jsonData.message;
-    		  			   var text = 'Error creating reason:\''+message+'\''
-    						Workspace.common.tool.Pop.failure(me, text);
-    	  			   }
-    	  			});
-        	    }
+        	Ext.Msg.show({
+    			prompt:true, 
+    			width: 400,
+    			buttons: Ext.Msg.YESNO,
+    			title:'Create directory in \''+itemPathDst+'\'', 
+    			msg:'Please enter a name:', 
+    			fn:function(btn, fileName) {
+	        	    if (btn == 'yes' && fileName != ''){
+	    		    	var requestUrl = DOMAIN_NAME_ROOT + '/action.servlet?event=FileBrowserNew';
+	    	  			Ext.Ajax.request({
+	    	  			   url: requestUrl,
+	    	  			   params: {type:typeNew, pathDst:itemPathDst, name:fileName},
+	    	  			   success: function(result, request){
+	    	  				   var text = 'Success creating \''+fileName+'\' in \''+itemPathDst+'\'';
+	    	  				   Workspace.common.tool.Pop.success(me, text);
+	
+	    	  				   // Rechargement de la grid
+	    	  				   var grid = mainCenterTab.items.items[0];
+	    	  				   grid.refresh();
+	
+	    	  				   // Rechargement du tree si besoin
+	    	  				   var application = Ext.getCmp('project').value;
+	    	  				   if ('['+application+']'==itemPathDst) {
+	    	  						var tree = Ext.getCmp("treeDirectory");
+	    	  						tree.getStore().load(
+		  								new Ext.data.Operation({
+		  									action:'read'
+		  								})
+		  							);
+	    	  				   }
+	    	  			   },
+	    	  			   failure: function (result, request) {
+	    		  			   var jsonData = Ext.decode(result.responseText);
+	    		  			   var message = jsonData.message;
+	    		  			   var text = 'Error creating \''+fileName+'\' reason:\''+message+'\''
+	    						Workspace.common.tool.Pop.failure(me, text);
+	    	  			   }
+	    	  			});
+	        	    }
+    			}
         	});
         }
 	}
