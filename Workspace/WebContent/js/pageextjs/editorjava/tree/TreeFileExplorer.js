b789619a69dee7bd84c59f6b3bb7142737c049d9
M6C <david.roca@free.fr>
2017-01-03 10:23:35 +0100

    Udp Editor Java Add Command Find Resource content on Tree File Explorer

--- Workspace/WebContent/js/pageextjs/editorjava/tree/TreeFileExplorer.js
+++ Workspace/WebContent/js/pageextjs/editorjava/tree/TreeFileExplorer.js
@@ -17,12 +17,15 @@ Ext.define('Workspace.editorjava.tree.TreeFileExplorer', {
 		Ext.Loader.syncRequire('Workspace.editorjava.panel.center.function.AddTabAce');
 		Workspace.editorjava.panel.center.function.AddTabAce.call(record.raw);
 	},
-	// Can be overrided
+	// Overrided
 	onItemKeyDown: function(view, record, item, index, event, eOpts) {
 		console.info('Workspace.editorjava.tree.TreeFileExplorer onItemKeyDown');
 		var superMethod = this.superclass.onItemKeyDown;
 		if (event.ctrlKey && event.shiftKey && event.keyCode == Ext.EventObject.R) {
-		    Workspace.editorjava.aceeditor.command.CommandFindResource.openFindResource();
+		    Workspace.editorjava.aceeditor.command.CommandFindResource.openFindResourceNameFilter(view);
+		    event.stopEvent();
+		} else if (event.ctrlKey && event.shiftKey && event.keyCode == Ext.EventObject.F) {
+		    Workspace.editorjava.aceeditor.command.CommandFindResource.openFindResourceContentFilter(view);
 		    event.stopEvent();
 		} else {
 		    superMethod(view, record, item, index, event, eOpts);
