0fc4cedb8c97afc8b523fc867600b71822c6fc85
David ROCA <david.roca@laposte.fr>
2017-01-24 15:09:03 +0100

    Upd Editor Java Add Debug Ability Part 2 - Fix static

--- Workspace/WebContent/js/pageextjs/editorjava/debug/ApplyDebug.js
+++ Workspace/WebContent/js/pageextjs/editorjava/debug/ApplyDebug.js
@@ -1,25 +1,27 @@
 Ext.define('Workspace.editorjava.debug.ApplyDebug', {
 
-	apply: function(editor) {
-	    console.debug('Workspace.editorjava.debug.ApplyDebug apply');
-
-        editor.on('guttermousedown', function(e) {
-            console.log(e.getDocumentPosition().row);
-            var target = e.domEvent.target; 
-            if (target.className.indexOf("ace_gutter-cell") == -1)
-                return;
-            if (!editor.isFocused()) 
-                return; 
-            if (e.clientX > 25 + target.getBoundingClientRect().left) 
-                return; 
-        
-            var breakpoints = e.editor.session.getBreakpoints();
-            var row = e.getDocumentPosition().row;
-            if(typeof breakpoints[row] === typeof undefined)
-                e.editor.session.setBreakpoint(row);
-            else
-                e.editor.session.clearBreakpoint(row);
-            e.stop();
-        })
+	statics: {
+    	apply: function(editor) {
+    	    console.debug('Workspace.editorjava.debug.ApplyDebug apply');
+    
+            editor.on('guttermousedown', function(e) {
+                console.log(e.getDocumentPosition().row);
+                var target = e.domEvent.target; 
+                if (target.className.indexOf("ace_gutter-cell") == -1)
+                    return;
+                if (!editor.isFocused()) 
+                    return; 
+                if (e.clientX > 25 + target.getBoundingClientRect().left) 
+                    return; 
+            
+                var breakpoints = e.editor.session.getBreakpoints();
+                var row = e.getDocumentPosition().row;
+                if(typeof breakpoints[row] === typeof undefined)
+                    e.editor.session.setBreakpoint(row);
+                else
+                    e.editor.session.clearBreakpoint(row);
+                e.stop();
+            })
+        }
     }
 }, function() {Workspace.tool.Log.defined('Workspace.editorjava.debug.ApplyDebug');});
