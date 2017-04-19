95f40168864afea71280e86b3cd11706918f9107
David ROCA <david.roca@laposte.fr>
2017-04-19 16:47:31 +0200

    Upd Editor Java Add South Console Panel Part 3

--- Workspace/WebContent/js/pageextjs/editorjava/panel/south/PanelConsole.js
+++ Workspace/WebContent/js/pageextjs/editorjava/panel/south/PanelConsole.js
@@ -1,6 +1,9 @@
 Ext.define('Workspace.editorjava.panel.south.PanelConsole', {
-
-	extend: 'Ext.panel.Panel'
+	requires: [
+  	     'Workspace.common.constant.ConstantKeyboard'
+  	]
+	,
+	extend: 'Ext.form.Panel'
 	,
 	alias: 'widget.panelDebugConsole',
 	alternateClassName: 'WorkspacePanelDebugConsole',
@@ -14,14 +17,48 @@ Ext.define('Workspace.editorjava.panel.south.PanelConsole', {
 
 		me.callParent(arguments);
 	},
-	layout: 'fit',
-    items: [{
+// 	layout: 'fit',
+    items: [
+        {
+            xtype     : 'hiddenfield',
+            // xtype     : 'textfield',
+            name      : 'commandLine',
+            id        : 'commandLine'
+        }
+        ,
+        {
         xtype     : 'textareafield',
         id        : 'fieldConsole',
         fieldCls  : 'console',
         grow      : true,
         name      : 'console',
-        anchor    : '100%'
+        anchor    : '100%',
+        enableKeyEvents : true,
+        listeners : {
+            keyup: function(me, event, eOpts) {
+        		var key = event.getKey();
+                var field = Ext.getCmp('commandLine');
+        	    if (key == event.ENTER) {
+                    field.setRawValue('');
+                    var form = me.up('form').getForm();
+                    form.submit({
+                        url : DOMAIN_NAME_ROOT + '/action.servlet?event=AdminPageExecCmdValiderExtJs',
+                        success: function(form, action) {
+                           Ext.Msg.alert('Success', action.result.msg);
+                        },
+                        failure: function(form, action) {
+                            Ext.Msg.alert('Failed', action.result.msg);
+                        }
+                    });
+        	    } else if (key == event.LEFT) {
+        	    } else if (key == event.RIGHT) {
+        	   // } else if ((64 <= key && key <= 90 /*ALPHA*/) || (48 <= key && key <= 57 /*NUMBER*/) || (96 <= key && key <= 105 /*NUMBER PAD*/) || (key == 32 /*SPACE*/)) {
+        	    } else if (!Ext.isEmpty(Workspace.common.constant.ConstantKeyboard.keyboardMapChar[key])) {
+        	        var char = event.browserEvent.key;
+                    field.setRawValue(field.getRawValue() + char);
+        	    }
+            }
+        }
     }]
     ,
     listenerShow: function(me, options) {
