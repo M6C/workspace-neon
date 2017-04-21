1f1e482f6444fe65d8f8c395e7998598e4e9ecd5
David ROCA <david.roca@laposte.fr>
2017-04-21 17:28:16 +0200

    Upd Editor Java Add South Console Panel Part 6

--- Workspace/src/workspace/service/SrvAdminExecuteCmd.java
+++ Workspace/src/workspace/service/SrvAdminExecuteCmd.java
@@ -19,44 +19,49 @@ import framework.trace.Trace;
 
 public class SrvAdminExecuteCmd extends SrvGenerique {
 
-    protected static final String OUT_PREFIX = "[OUT] ";
-    protected static final String ERR_PREFIX = "[ERR] ";
+    protected static final String OUT_PREFIX = "[OUT]";
+    protected static final String ERR_PREFIX = "[ERR]";
 
 	public void execute(HttpServletRequest request, HttpServletResponse response, BeanGenerique bean) throws Exception {
         String commandLine = (String) bean.getParameterDataByName("commandLine");
         String result = "";
-    	File cmdFile = File.createTempFile("Exec_Cmd_" + Long.toString(new Date().getTime()), ".cmd");
-    	String line = cmdFile.getAbsolutePath(); //"cmd /C " + cmdFile.getAbsolutePath();
+    	String line = "";
         try {
             if (UtilString.isNotEmpty(commandLine)) {
                 StringBuffer stb = new StringBuffer();
 
-            	FileWriter out = new FileWriter(cmdFile);
+        	    StringBuffer sbCmd = new StringBuffer();
+            	for(String cmd : commandLine.split("\n")) {
+            		cmd = cmd.trim();
+            		if (UtilString.isNotEmpty(cmd) && !cmd.startsWith(OUT_PREFIX) && !cmd.startsWith(ERR_PREFIX)) {
+    			        sbCmd.append(cmd + "\r\n");
+            		}
+            	}
+
+                FileWriter out = null;
             	try {
-	            	for(String cmd : commandLine.split("\n")) {
-	            		cmd = cmd.trim();
-	            		if (UtilString.isNotEmpty(cmd) && !cmd.startsWith(OUT_PREFIX) && !cmd.startsWith(ERR_PREFIX)) {
-	            			out.append(cmd + "\r\n");
-	            		}
-	            	}
-            		out.close();
-            		out = null;
-	
-					execCmd(line, stb);
-	
-	            	result = stb.toString();
+                	File cmdFile = File.createTempFile("Exec_Cmd_" + Long.toString(new Date().getTime()), ".cmd");
+            	    out = new FileWriter(cmdFile);
+        			out.write(sbCmd.toString());
+        			out.close();
+        			out = null;
+
+                	line = cmdFile.getAbsolutePath(); //"cmd /C " + cmdFile.getAbsolutePath();
+				    execCmd(line, stb);
             	} finally {
             		if (out != null) {
             			out.close();
             		}
             	}
+
+            	result = stb.toString();
             }
         }
         catch(Exception ex) {
             ByteArrayOutputStream streamLog = new ByteArrayOutputStream();
             PrintStream psLog = new PrintStream(streamLog);
             ex.printStackTrace(psLog);
-            result = ERR_PREFIX + streamLog.toString();
+            result = ERR_PREFIX + " " + streamLog.toString();
             Trace.ERROR(this, ex);
         } finally {
         	if (UtilString.isEmpty(result)) {
@@ -80,7 +85,7 @@ public class SrvAdminExecuteCmd extends SrvGenerique {
             if (stb != null) {
                 BufferedReader out = new BufferedReader(new InputStreamReader(p.getInputStream()));
                 for(String str = out.readLine(); str != null; str = out.readLine()) {
-                    stb.append(OUT_PREFIX).append(str).append("\r\n");
+                    stb.append(OUT_PREFIX).append(" ").append(str).append("\r\n");
                 }
             }
             p.destroy();
